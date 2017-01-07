03efebd520a81be39b7c9c4da91a48151eba56d2
Uzumaki-C <15951858526@163.com>
2017-01-07 13:48:55 +0800

    link

---
+++ src/Classic/string/space_res/one_node_del.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Remove:
+    def removeNode(self, pNode):
+        # write code here
+        if pNode is None or not pNode.next:
+            return False
+        else:
+            pNode.val = pNode.next.val
+            pNode.next = pNode.next.next
+            return True
+
+
+
+# 题目描述
+# 实现一个算法，删除单向链表中间的某个结点，假定你只能访问该结点。
+# 给定带删除的节点，请执行删除操作，若该节点为尾节点，返回false，否则返回true
